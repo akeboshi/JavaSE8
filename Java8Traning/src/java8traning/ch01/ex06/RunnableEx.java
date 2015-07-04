1f3c01d61f6b7ea5e4e6645499ae15e570d85086
akeboshi <akari.akeboshi@gmail.com>
2015-07-04 19:08:51 +0900

    add 5,6

---
+++ Java8Traning/src/java8traning/ch01/ex06/RunnableEx.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java8traning.ch01.ex06;
+
+/**
+ *
+ * @author akari
+ */
+@FunctionalInterface
+public interface RunnableEx {
+    public void run () throws Exception;
+}
---
+++ Java8Traning/src/java8traning/ch01/ex06/ThreadUncheck.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java8traning.ch01.ex06;
+
+/**
+ *
+ * @author akari
+ */
+public class ThreadUncheck {
+
+    public static void main(String arg[]) {
+        new ThreadUncheck().threadUncheck();
+    }
+
+    public void threadUncheck() {
+        new Thread(uncheck(() -> {
+            System.out.println("Zzz");
+            Thread.sleep(1000);
+        })).start();
+    }
+
+    public static Runnable uncheck(RunnableEx re) throws RuntimeException {
+        return () -> {
+            try {
+                re.run();
+            } catch (Exception e) {
+                throw new RuntimeException();
+            }
+        };
+    }
+}
