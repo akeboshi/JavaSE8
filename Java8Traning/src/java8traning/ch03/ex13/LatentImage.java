ee609db8e5f9f689399a60ca48be757d1d35e25c
akeboshi <akari.akeboshi@gmail.com>
2015-08-13 18:06:29 +0900

    add 03-13

---
+++ Java8Traning/src/java8traning/ch03/ex13/LatentImage.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2015. Isao Aruga. All rights reserved.
+ */
+
+package java8traning.ch03.ex13;
+
+import java8traning.ch03.ex05.ColorTransformer;
+import javafx.scene.image.Image;
+import javafx.scene.image.WritableImage;
+import javafx.scene.paint.Color;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.UnaryOperator;
+
+/**
+ * Created by akari on 2015/08/12.
+ */
+public class LatentImage {
+    private Image in;
+    private List<ImageTransformer> pendingOperations = new ArrayList<>();
+
+    private LatentImage() {
+    }
+
+    private void setInputImage(Image in) {
+        this.in = in;
+    }
+
+    public LatentImage transform(UnaryOperator<Color> f) {
+        pendingOperations.add((x, y, image) -> f.apply(image[x][y]));
+        return this;
+    }
+
+    public LatentImage transform(ColorTransformer f) {
+        pendingOperations.add((x, y, image) -> f.apply(x, y, image[x][y]));
+        return this;
+    }
+
+    public LatentImage transform(ImageTransformer f) {
+        pendingOperations.add(f);
+        return this;
+    }
+
+    public static LatentImage from(Image in) {
+        LatentImage img = new LatentImage();
+        img.setInputImage(in);
+        return img;
+    }
+
+    public Image toImage() {
+        int width = (int) in.getWidth();
+        int height = (int) in.getHeight();
+        WritableImage out = new WritableImage(width, height);
+        Color[][] image = new Color[width][height];
+        for (int x = 0; x < width; x++)
+            for (int y = 0; y < height; y++)
+                image[x][y] = in.getPixelReader().getColor(x, y);
+
+
+        for (int x = 0; x < width; x++)
+            for (int y = 0; y < height; y++) {
+                for (ImageTransformer f : pendingOperations)
+                    image[x][y] = f.apply(x, y, image);
+                out.getPixelWriter().setColor(x, y, image[x][y]);
+            }
+        return out;
+    }
+}
---
+++ Java8Traning/src/java8traning/ch03/ex13/LatentImageApp.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2015. Isao Aruga. All rights reserved.
+ */
+
+package java8traning.ch03.ex13;
+
+import java8traning.ch03.ex05.ColorTransformer;
+import javafx.application.Application;
+import javafx.geometry.Orientation;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+/**
+ * Created by akari on 2015/08/11.
+ */
+public class LatentImageApp extends Application {
+    static public void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Image image = new Image("test.png");
+        int width = (int) image.getWidth();
+        int height = (int) image.getHeight();
+        ColorTransformer grayFrame = (x, y, color) -> {
+            if (x < 10 || y < 10 || x > width - 10 || y > height - 10) return Color.GRAY;
+            return color;
+        };
+        ImageTransformer avgFilter = (x, y, in) -> {
+            double r = 0, g = 0, b = 0;
+            double n = 0;
+            for (int i = -1; i < 1; i++)
+                for (int j = -1; j < 1; j++) {
+                    if (x + i >= 0 && y + j >= 0) {
+                        n++;
+                        r += in[x + i][y + j].getRed();
+                        g += in[x + i][y + j].getGreen();
+                        b += in[x + i][y + j].getBlue();
+                    }
+                }
+            return Color.color(r / n, g / n, b / n);
+        };
+        Image transFormed = LatentImage.from(image)
+                .transform(avgFilter)
+                .transform(Color::grayscale)
+                .transform(grayFrame)
+                .toImage();
+        FlowPane flowPane = new FlowPane(new ImageView(image), new ImageView(transFormed));
+        // 方向を設定する(Orientation.VERTICAL : 垂直／Orientation.HORIZONTAL : 水平)
+        flowPane.setOrientation(Orientation.HORIZONTAL);
+
+        stage.setScene(new Scene(flowPane, width * 2, height * 2, Color.WHITE));
+        stage.show();
+    }
+}
